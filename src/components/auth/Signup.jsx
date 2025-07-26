dbb75b719531f68fc0864a0b3c6ff505c030797f
kiran tandel <tandelakki111@gmail.com>
2025-07-26 23:25:42 +0530

    Connected frontend to live backend

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -43,7 +43,7 @@ const Signup = () => {
 
     try {
       dispatch(setLoading(true));
-      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+      const res = await axios.post(`https://job-portal-backend-1-grrb.onrender.com/register`, formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
         withCredentials: true,
       });
